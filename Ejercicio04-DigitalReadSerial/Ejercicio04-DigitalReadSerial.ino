f7481f06ce387ebd9e828bdc66f7d33a3e4e13ec
Enrique Crespo <ecrespo@arsys.es>
2014-11-26 11:52:08 +0100

    Ejercicio 4

---
+++ Ejercicio04-DigitalReadSerial/Ejercicio04-DigitalReadSerial.ino
@@ -0,0 +1,13 @@
+int pushButton = 2;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(pushButton);
+  Serial.println(buttonState);
+  delay(1);
+}
