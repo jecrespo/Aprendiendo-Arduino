21cf1a8e6d3a06a7c987e03df8aec8028ec4bf13
Enrique Crespo <ecrespo@arsys.es>
2014-12-04 14:57:05 +0100

    Ejercicio 19 final

--- Ejercicio19-BlinkSinDelay/Ejercicio19-BlinkSinDelay.ino
+++ Ejercicio19-BlinkSinDelay/Ejercicio19-BlinkSinDelay.ino
@@ -16,19 +16,22 @@
  by Paul Stoffregen
  
  This example code is in the public domain.
-
+ 
  
  http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
  */
- 
-/////
-// TODO: Multitarea y a la vez recoger datos de sonda de temperatura o un boton.
-// Usar librería timer2 y otra
-/////
+
+#include "Timer.h"
+#include <MsTimer2.h>
+
+Timer t;
 
 // constants won't change. Used here to 
 // set pin numbers:
 const int ledPin =  13;      // the number of the LED pin
+const int ledPin2 =  10;
+const int buttonPin = 9;
+boolean buttonState;
 
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
@@ -37,33 +40,61 @@ long previousMillis = 0;        // will store last time LED was updated
 // the follow variables is a long because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long interval = 1000;           // interval at which to blink (milliseconds)
+int count = 0;
 
 void setup() {
+  Serial.begin(9600);
   // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);      
-}
-
-void loop()
-{
-  // here is where you'd put code that needs to be running all the time.
-
-  // check to see if it's time to blink the LED; that is, if the 
-  // difference between the current time and last time you blinked 
-  // the LED is bigger than the interval at which you want to 
-  // blink the LED.
-  unsigned long currentMillis = millis();
- 
-  if(currentMillis - previousMillis > interval) {
-    // save the last time you blinked the LED 
-    previousMillis = currentMillis;   
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW)
-      ledState = HIGH;
-    else
-      ledState = LOW;
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);  
+  pinMode(buttonPin, INPUT_PULLUP);
+  MsTimer2::set(5000,blinkLed2);  //http://playground.arduino.cc/Main/MsTimer2
+  MsTimer2::start();
+  t.every(3000,printCount);
+  buttonState = digitalRead(buttonPin);
   }
+
+  void loop()
+  {
+    // here is where you'd put code that needs to be running all the time.
+
+    // check to see if it's time to blink the LED; that is, if the 
+    // difference between the current time and last time you blinked 
+    // the LED is bigger than the interval at which you want to 
+    // blink the LED.
+    t.update();
+    unsigned long currentMillis = millis();
+
+    if (buttonState != digitalRead(buttonPin)){
+      count++;
+      buttonState = !buttonState;
+    }
+
+    if(currentMillis - previousMillis > interval) {
+      // save the last time you blinked the LED 
+      previousMillis = currentMillis;   
+
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW)
+        ledState = HIGH;
+      else
+        ledState = LOW;
+
+      // set the LED with the ledState of the variable:
+      digitalWrite(ledPin, ledState);
+    }
+  }
+
+void printCount(){
+  Serial.print("He contado ");
+  Serial.print(count);
+  Serial.println(" pulsaciones de boton");
 }
+
+void blinkLed2(){
+  static boolean ledState = LOW;
+  digitalWrite(ledPin2, ledState);
+  ledState = !ledState;
+}
+
+
