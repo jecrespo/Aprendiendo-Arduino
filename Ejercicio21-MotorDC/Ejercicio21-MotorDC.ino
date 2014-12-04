93da9842944afb65d74afb70061170d794f44dc5
Enrique Crespo <ecrespo@arsys.es>
2014-12-04 15:20:36 +0100

    Ejercicio 21 fin

--- Ejercicio21-MotorDC/Ejercicio21-MotorDC.ino
+++ Ejercicio21-MotorDC/Ejercicio21-MotorDC.ino
@@ -1,4 +1,5 @@
 //Ejercicio basado en los ejercicios 9 y 10 del libro Arduino Starter Kit
+#define SEGURIDAD 1
 
 //const int switchPin = 10;
 const int controlPin1 = 2;
@@ -7,6 +8,7 @@ const int enablePin = 9;
 const int directionSwitchPin = 4;
 const int onOffSwitchStateSwitchPin = 5;
 const int potPin = A0;
+const int emergenciaPin = 11;
 
 int onOffSwitchState = 0;
 int previousOnOffSwitchState = 0;
@@ -22,62 +24,112 @@ int motorDirection = 1;
 //encendido_apagado = 0;
 
 void setup(){
-//pinMode(motorPin, OUTPUT);
-//pinMode(switchPin, INPUT);
-pinMode(directionSwitchPin, INPUT);
-pinMode(onOffSwitchStateSwitchPin, INPUT);
-pinMode(controlPin1, OUTPUT);
-pinMode(controlPin2, OUTPUT);
-pinMode(enablePin, OUTPUT);
-digitalWrite(enablePin, LOW);
+  //pinMode(motorPin, OUTPUT);
+  //pinMode(switchPin, INPUT);
+  pinMode(directionSwitchPin, INPUT);
+  pinMode(onOffSwitchStateSwitchPin, INPUT);
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  pinMode(emergenciaPin, INPUT_PULLUP);
+  digitalWrite(enablePin, LOW);
 }
 
 void loop(){
-// leo el boton de encendido/apagado del pin 2
-/*switchState = digitalRead(switchPin);
-if ((switchState == HIGH)&& !encendido_apagado){	//Si está el motor apagado y leo un pulso
-	encendido_apagado = 1;
-	digitalWrite(motorPin, HIGH);
-}
-if (switchState == HIGH)&& encendido_apagado){	//Si está el motor encendido y leo un pulso
-	encendido_apagado = 0;
-	digitalWrite(motorPin, LOW);
-}
-*/
-onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
-delay(1);
-directionSwitchState = digitalRead(directionSwitchPin);
-motorSpeed = analogRead(potPin)/4;
+  // leo el boton de encendido/apagado del pin 2
+  /*switchState = digitalRead(switchPin);
+   if ((switchState == HIGH)&& !encendido_apagado){	//Si estï¿½ el motor apagado y leo un pulso
+   	encendido_apagado = 1;
+   	digitalWrite(motorPin, HIGH);
+   }
+   if (switchState == HIGH)&& encendido_apagado){	//Si estï¿½ el motor encendido y leo un pulso
+   	encendido_apagado = 0;
+   	digitalWrite(motorPin, LOW);
+   }
+   */
+  if (digitalRead(emergenciaPin) == HIGH){
+    onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+    delay(1);
+    directionSwitchState = digitalRead(directionSwitchPin);
+    motorSpeed = analogRead(potPin)/4;
 
-if (onOffSwitchState != previousOnOffSwitchState){
-	if (onOffSwitchState == HIGH){
-		motorEnabled = !motorEnabled;
-	}
+    if (onOffSwitchState != previousOnOffSwitchState){
+      if (onOffSwitchState == HIGH){
+        motorEnabled = !motorEnabled;
+      }
+    }
+
+    if (directionSwitchState != previousDirectionSwitchState) {
+      if (directionSwitchState == HIGH){
+        motorDirection = !motorDirection;
+      }
+    }
+
+    if (motorDirection == 1) {
+#if SEGURIDAD
+      //Parada en rampa
+      for (int i = motorSpeed; i > 0; i--){
+        analogWrite(enablePin,i);
+        delay(5);
+      }
+#endif
+      digitalWrite(controlPin1, HIGH);
+      digitalWrite(controlPin2, LOW);
+#if SEGURIDAD
+      //Arranque en rampa
+      for (int i = 0; i < motorSpeed; i++){
+        analogWrite(enablePin,i);
+        delay(5);
+      }
+#endif   
+    }
+    else {
+#if SEGURIDAD
+      //Parada en rampa
+      for (int i = motorSpeed; i > 0; i--){
+        analogWrite(enablePin,i);
+        delay(5);
+      }
+#endif 
+      digitalWrite(controlPin1,LOW);
+      digitalWrite(controlPin2,HIGH);
+#if SEGURIDAD
+      //Arranque en rampa
+      for (int i = 0; i < motorSpeed; i++){
+        analogWrite(enablePin,i);
+        delay(5);
+      }
+#endif    
+    }
+
+    if (motorEnabled == 1){
+#if SEGURIDAD
+      //Arranque en rampa
+      for (int i = 0; i < motorSpeed; i++){
+        analogWrite(enablePin,i);
+        delay(5);
+      }
+#endif    
+    }
+    else {
+#if SEGURIDAD
+      //Parada en rampa
+      for (int i = motorSpeed; i > 0; i--){
+        analogWrite(enablePin,i);
+        delay(5);
+      }
+#endif
+    }
+
+    previousDirectionSwitchState = directionSwitchState;
+    previousOnOffSwitchState = onOffSwitchState;
+  }
+  else {
+    analogWrite(enablePin,0);
+  }
 }
 
-if (directionSwitchState != previousDirectionSwitchState) {
-	if (directionSwitchState == HIGH){
-		motorDirection = !motorDirection;
-	}
-}
 
-if (motorDirection == 1) {
-	digitalWrite(controlPin1, HIGH);
-	digitalWrite(controlPin2, LOW);
-}
-else {
-	digitalWrite(controlPin1,LOW);
-	digitalWrite(controlPin2,HIGH);
-}
 
-if (motorEnabled == 1){
-	analogWrite(enablePin,motorSpeed);
-}
-else {
-	analogWrite(enablePin, 0);
-}
 
-previousDirectionSwitchState = directionSwitchState;
-previousOnOffSwitchState = onOffSwitchState;
 
-}
