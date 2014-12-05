c64b7e26157c8ffb6563584381dbc9b65746dd2d
Enrique Crespo <ecrespo@arsys.es>
2014-12-05 12:19:35 +0100

    Ejercicio 20 fin.

--- Ejercicio20-Debug/Ejercicio20-Debug.ino
+++ Ejercicio20-Debug/Ejercicio20-Debug.ino
@@ -1,34 +1,85 @@
-/* Version 0.1 beta */
+/* Version 0.2 */
 /////
-// TODO: Comprobar tiempos de loop, tamaño en flash y uso de memoria con la opcion de debug y verbose
+// Comprobar tamaï¿½o en flash al compilar.
 /////
 
-#define DEBUG 0
-#define VERBOSE 0
+#define DEBUG 0		//para hacer debug
+#define VERBOSE 0	//saca mï¿½s informaciï¿½n
+#define ERRORES 0	//Provoco errores
+unsigned long tiempo;
+
+#if DEBUG	//Esto harï¿½ un consumo alto de memoria
+unsigned long datos[50];
+#endif
+
+#if ERRORES
+serial.println(errores)
+ajs qweqwe;
+int variable = "error"
+#endif
 
 void setup(){
   Serial.println(inicializa());
+  Serial.println(freeRam());
+#if DEBUG  
+  for (int i=0; i<50; i++){
+    datos[i] = i;
+  }
+  Serial.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");  //Ocupa memoria en flash
+#endif
 }
 void loop(){
-    Serial.println(compruebaTemperatura());
-  #if DEBUG
-    imprimeErrores();
-  #endif
+  Serial.println(freeRam());
+#if VERBOSE
+  Serial.print("Tiempo de loop: ");
+  Serial.print(millis()-tiempo);
+  Serial.println(" ms");
+  tiempo = millis();
+#endif
+  float temperatura = compruebaTemperatura();
+#if DEBUG	//Decido si saco errores por serie o no y retrasa el programa para que vaya mï¿½s lento
+  imprimeErrores(temperatura);
+  retrasaPrograma();
+#endif
 }
 
 String inicializa() {
-	String inicio = "Inicializando...";
-	return inicio;
+  String inicio = "Inicializando...";
+  return inicio;
 }
 
 float compruebaTemperatura() {
-	//Leer temperatura
-	//Hacer acciones
-	#if DEBUG
-	Serial.println("Tarea finalizada");
-	#endif
+  float temperatura = analogRead(A0)*5/1024;
+#if VERBOSE
+  Serial.println("Tarea de lectura de temperatura finalizada...");
+#endif
+  return temperatura;
 }
 
-void imprimeErrores(){
-	Serial.println("Errores...");
+void imprimeErrores(float temp){
+  if ((temp > 4)||(temp <1)){
+    Serial.println("Error de temperatura!!!!!!!");
+  }
 }
+
+void retrasaPrograma(){
+  delay(1000);
+}
+
+int freeRam () 
+{
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
