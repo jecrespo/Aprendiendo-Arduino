e885cffcc29e3eab6ff36723e94bc1f7a2854b19
Enrique Crespo <ecrespo@arsys.es>
2014-12-12 09:57:15 +0100

    Ejercicio 25, 26 y 27

---
+++ Ejercicio25-EthernetClient/Ejercicio25-EthernetClient.ino
@@ -0,0 +1,67 @@
+#include <Ethernet.h>
+#include <SPI.h>
+
+
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 
+  192, 168, 1, 179 };
+byte server[] = { 
+  64, 233, 187, 99 }; // Google
+byte DNS[] = {
+  8,8,8,8};
+byte gateway[] = {
+  192, 168, 1, 1};
+byte subnet[] = {
+  255,255,255,0};
+byte aemet[] = {
+  172,24,34,57};
+byte google[] = { 64, 233, 187, 99 }; 
+
+
+EthernetClient client;
+String webString = "";
+//char url[] = "aemet.es";
+//char api[] = "api.openweathermap.org";
+
+void setup()
+{
+  Ethernet.begin(mac, ip, DNS, gateway, subnet);
+  Serial.begin(9600);
+
+  delay(1000);
+
+  Serial.println("connecting...");
+
+  if (client.connect(google, 80)) {
+    Serial.println("connected");
+    client.println("GET / HTTP/1.0");
+    //client.println("GET /data/2.5/weather?q=London&mode=xml HTTP/1.1");
+    //client.println("GET /es/portada HTTP/1.1");
+    //client.println("GET / HTTP/1.1");
+    //client.println("GET /xml/municipios/localidad_26089.xml HTTP/1.1");
+    client.println();
+  } 
+  else {
+    Serial.println("connection failed");
+  }
+}
+
+void loop()
+{
+  if (client.available()) {
+    char c = client.read();
+    webString += c;
+    Serial.print(c);
+  }
+
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    Serial.println(webString);
+    for(;;)
+      ;
+  }
+}
+
---
+++ Ejercicio26-EthernetServer/Ejercicio26-EthernetServer.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+// network configuration.  gateway and subnet are optional.
+
+ // the media access control (ethernet hardware) address for the shield:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
+//the IP address for the shield:
+byte ip[] = { 10, 0, 0, 177 };    
+// the router's gateway address:
+byte gateway[] = { 10, 0, 0, 1 };
+// the subnet:
+byte subnet[] = { 255, 255, 0, 0 };
+
+// telnet defaults to port 23
+EthernetServer server = EthernetServer(23);
+
+void setup()
+{
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip, gateway, subnet);
+
+  // start listening for clients
+  server.begin();
+}
+
+void loop()
+{
+  // if an incoming client connects, there will be bytes available to read:
+  EthernetClient client = server.available();
+  if (client == true) {
+    // read bytes from the incoming client and write them back
+    // to any clients connected to the server:
+    server.write(client.read());
+  }
+}
