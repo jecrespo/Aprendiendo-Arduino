0efac0a73aa5cb33efb7dd66b13f58fcce470759
Enrique Crespo <jecrespom@gmail.com>
2015-03-26 10:40:34 +0100

    Update Ejercicio01-Blink.ino

--- Ejercicio01-Blink/Ejercicio01-Blink.ino
+++ Ejercicio01-Blink/Ejercicio01-Blink.ino
@@ -8,7 +8,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("Encendio");
+  Serial.println("Encendido");
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   Serial.println("Apagado");
