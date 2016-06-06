46c6cff7e9684688cccc52537357e232c16a3ee5
Jose Enrique Crespo <Jose Enrique Crespo>
2016-06-06 22:05:13 +0200

    Mejora ejercicio 17

--- Ejercicio17-CocheFantastico_2/Ejercicio17-CocheFantastico_2.ino
+++ Ejercicio17-CocheFantastico_2/Ejercicio17-CocheFantastico_2.ino
@@ -1,37 +1,39 @@
 /* Knight Rider 2
- * --------------
- *
- * Reducing the amount of code using for(;;).
- *
- *
- * (cleft) 2005 K3, Malmo University
- * @author: David Cuartielles
- * @hardware: David Cuartielles, Aaron Hallborg
- */
+   --------------
+
+   Reducing the amount of code using for(;;).
+
+
+   (cleft) 2005 K3, Malmo University
+   @author: David Cuartielles
+   @hardware: David Cuartielles, Aaron Hallborg
+*/
 
 int pinArray[] = {2, 3, 4, 5, 6, 7};
 int count = 0;
 int timer = 100;
 
-void setup(){
+void setup() {
   // we make all the declarations at once
-  for (count=0;count<6;count++) {
+  for (count = 0; count < 6; count++) {
     pinMode(pinArray[count], OUTPUT);
   }
 }
 
 void loop() {
   //timer = analogRead(A0);	//El valor leido por analog read es el temporizador
-  for (count=0;count<6;count++) {
-   digitalWrite(pinArray[count], HIGH);
-   delay(timer);
-   digitalWrite(pinArray[count], LOW);
-   delay(timer);
+  for (count = 0; count < 6; count++) {
+    //timer = analogRead(A0);
+    digitalWrite(pinArray[count], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count], LOW);
+    delay(timer);
   }
-  for (count=5;count>=0;count--) {
-   digitalWrite(pinArray[count], HIGH);
-   delay(timer);
-   digitalWrite(pinArray[count], LOW);
-   delay(timer);
+  for (count = 5; count >= 0; count--) {
+    //timer = analogRead(A0);
+    digitalWrite(pinArray[count], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count], LOW);
+    delay(timer);
   }
 }
--- Ejercicio17-CocheFantastico_3/Ejercicio17-CocheFantastico_3.ino
+++ Ejercicio17-CocheFantastico_3/Ejercicio17-CocheFantastico_3.ino
@@ -1,41 +1,41 @@
 /* Knight Rider 3
- * --------------
- *
- * This example concentrates on making the visuals fluid.
- *
- *
- * (cleft) 2005 K3, Malmo University
- * @author: David Cuartielles
- * @hardware: David Cuartielles, Aaron Hallborg
- */
+   --------------
+
+   This example concentrates on making the visuals fluid.
+
+
+   (cleft) 2005 K3, Malmo University
+   @author: David Cuartielles
+   @hardware: David Cuartielles, Aaron Hallborg
+*/
 
 int pinArray[] = {2, 3, 4, 5, 6, 7};
 int count = 0;
 int timer = 30;
 
-void setup(){
-  for (count=0;count<6;count++) {
+void setup() {
+  for (count = 0; count < 6; count++) {
     pinMode(pinArray[count], OUTPUT);
   }
 }
 
 void loop() {
-  for (count=0;count<5;count++) {
-   digitalWrite(pinArray[count], HIGH);
-   delay(timer);
-   digitalWrite(pinArray[count + 1], HIGH);
-   delay(timer);
-   digitalWrite(pinArray[count], LOW);
-   delay(timer*2);
-   //timer = analogRead(A0);	//El valor leido por analog read es el temporizador
+  for (count = 0; count < 5; count++) {
+    digitalWrite(pinArray[count], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count + 1], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count], LOW);
+    delay(timer * 2);
+    //timer = analogRead(A0);	//El valor leido por analog read es el temporizador
   }
-  for (count=5;count>0;count--) {
-   digitalWrite(pinArray[count], HIGH);
-   delay(timer);
-   digitalWrite(pinArray[count - 1], HIGH);
-   delay(timer);
-   digitalWrite(pinArray[count], LOW);
-   delay(timer*2);
-   //timer = analogRead(A0);	//El valor leido por analog read es el temporizador
+  for (count = 5; count > 0; count--) {
+    digitalWrite(pinArray[count], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count - 1], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count], LOW);
+    delay(timer * 2);
+    //timer = analogRead(A0);	//El valor leido por analog read es el temporizador
   }
 }
